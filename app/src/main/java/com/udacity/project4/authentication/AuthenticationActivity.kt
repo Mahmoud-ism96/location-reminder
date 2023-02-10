ce06f19612a7bc52b1691ec3d203b2353305a13b
mahmoud.ahm.ism <mah.ahm.ism@gmail.com>
2023-02-10 16:53:39 +0200

    Updated Auth Login/Logout

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,15 +1,18 @@
 package com.udacity.project4.authentication
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.AuthUI.IdpConfig.*
+import com.firebase.ui.auth.AuthUI.IdpConfig.EmailBuilder
+import com.firebase.ui.auth.AuthUI.IdpConfig.GoogleBuilder
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
-import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityAuthenticationBinding
-import java.util.*
+import com.udacity.project4.locationreminders.RemindersActivity
 
 
 /**
@@ -44,6 +47,7 @@ class AuthenticationActivity : AppCompatActivity() {
     private val signInLauncher = registerForActivityResult(
         FirebaseAuthUIActivityResultContract()
     ) { result ->
+        Log.i("Authentication", "Result: ${result}!")
         this.onSignInResult(result)
     }
 
@@ -51,12 +55,16 @@ class AuthenticationActivity : AppCompatActivity() {
         val response = result.idpResponse
         if (result.resultCode == RESULT_OK) {
             val user = FirebaseAuth.getInstance().currentUser
+            Log.i("Authentication", "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+            Toast.makeText(this, "Successfully signed in", Toast.LENGTH_SHORT).show()
+            startReminderActivity()
 
         } else {
             // Sign in failed. If response is null the user canceled the
             // sign-in flow using the back button. Otherwise check
             // response.getError().getErrorCode() and handle the error.
-            // ...
+            Log.i("Authentication", "Sign in unsuccessful ${response?.error?.errorCode}")
+            Toast.makeText(this, "Sign in unsuccessful", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -64,7 +72,7 @@ class AuthenticationActivity : AppCompatActivity() {
     private fun startSignIn() {
 
         var signInIntent = AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
-            Arrays.asList(
+            listOf(
                 GoogleBuilder().build(),
                 EmailBuilder().build()
             )
@@ -73,5 +81,9 @@ class AuthenticationActivity : AppCompatActivity() {
         signInLauncher.launch(signInIntent)
     }
 
+    private fun startReminderActivity() {
+        finish()
+        startActivity(Intent(this, RemindersActivity::class.java))
+    }
 
 }
