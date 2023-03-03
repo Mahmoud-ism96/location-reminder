8e66ede6c40f5085ad937dadf26d5d032b035742
mahmoud.ahm.ism <mah.ahm.ism@gmail.com>
2023-03-03 18:00:38 +0200

    Added RemindersLocalRepository Tests

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -11,12 +11,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.not
+import org.junit.*
 import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
@@ -25,6 +23,83 @@ import org.junit.runner.RunWith
 @MediumTest
 class RemindersLocalRepositoryTest {
 
-//    TODO: Add testing implementation to the RemindersLocalRepository.kt
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var localDataSource: RemindersLocalRepository
+    private lateinit var database: RemindersDatabase
+
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(), RemindersDatabase::class.java
+        ).allowMainThreadQueries().build()
+
+        localDataSource = RemindersLocalRepository(
+            database.reminderDao(), Dispatchers.Main
+        )
+    }
+
+    @After
+    fun cleanUp() {
+        database.close()
+    }
+
+    @Test
+    fun saveReminderAndGetById() = runBlocking {
+        val reminder =
+            ReminderDTO("Yoga", "Yoga Session", "Flexana", 30.011642516567647, 31.4382229282943)
+        localDataSource.saveReminder(reminder)
+
+        val loaded = localDataSource.getReminder(reminder.id)
+
+        assertThat(loaded is Result.Success, `is`(true))
+        loaded as Result.Success
+
+        assertThat(loaded.data.id, Matchers.`is`(reminder.id))
+        assertThat(loaded.data.title, Matchers.`is`(reminder.title))
+        assertThat(loaded.data.description, Matchers.`is`(reminder.description))
+        assertThat(loaded.data.location, Matchers.`is`(reminder.location))
+        assertThat(loaded.data.latitude, Matchers.`is`(reminder.latitude))
+        assertThat(loaded.data.longitude, Matchers.`is`(reminder.longitude))
+    }
+
+    @Test
+    fun getReminders() = runBlocking {
+        val reminder =
+            ReminderDTO("Yoga", "Yoga Session", "Flexana", 30.011642516567647, 31.4382229282943)
+        val reminder2 =
+            ReminderDTO("Yoga1", "Yoga Session", "Flexana", 30.011642516567647, 31.4382229282943)
+
+        localDataSource.saveReminder(reminder)
+        localDataSource.saveReminder(reminder2)
+
+        val remindersList = localDataSource.getReminders()
+
+        assertThat(remindersList is Result.Success, `is`(true))
+        remindersList as Result.Success
+
+        assertThat(remindersList.data, `is`(not(emptyList())))
+    }
+
+    @Test
+    fun deleteAllReminders() = runBlocking {
+        val reminder =
+            ReminderDTO("Yoga", "Yoga Session", "Flexana", 30.011642516567647, 31.4382229282943)
+
+        localDataSource.saveReminder(reminder)
+
+        localDataSource.deleteAllReminders()
+
+        val remindersList = localDataSource.getReminders()
+
+        assertThat(remindersList is Result.Success, `is`(true))
+        remindersList as Result.Success
+
+        assertThat(remindersList.data, `is`(emptyList()))
+    }
+
+
 
 }
